593280857eb0e74e44b98334168db40537498ccc
khalp570 <khalp570@newschool.edu>
2018-04-02 17:31:26 -0400

    upload again

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 $(function() {
-	var $inputs = $('input[type="checkbox"]');
+  var $inputs = $('input[type="checkbox"]');
 
-	$inputs.change(function() {
-		var $input = $(this);
-		var text = $input.attr('data-text');
-		var destinationId = $input.attr('name');
-		var $destination = $('[data-text-destination="' + destinationId + '"]');
+  $inputs.change(function() {
+    var $input = $(this);
+    var text = $input.attr('data-text');
+    var destinationId = $input.attr('name');
+    var $destination = $('[data-text-destination="' + destinationId + '"]');
 
-		console.log('$destination', $destination);
-		console.log('text', text);
 
-		if ( $input.is(':checked') ) {
-			$destination.html(text);
-		} else {
-			$destination.empty();
-		}
-	});
+    var $destinationLink = $('[data-link-destination="' + destinationId + '"]');
+  
+    console.log('$destination', $destination);
+    console.log('text', text);
+
+    if ( $input.is(':checked') ) {
+      $destination.show();
+      $destinationLink.show();
+    } else {
+      $destination.hide();
+      $destinationLink.hide();
+    }
+  });
 });
