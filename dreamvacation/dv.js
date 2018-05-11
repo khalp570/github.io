fbdf530e35fc1e1700cd6b56544ddf76c42a0f94
khalp570 <khalp570@newschool.edu>
2018-05-11 02:01:21 -0400

    dream vacation

--- dreamvacation/dv.js
+++ dreamvacation/dv.js
@@ -1,42 +1,25 @@
+$(function() {
+  var frameNumber = 0; // start video at frame 0
+  // lower numbers = faster playback
+  var playbackConst = 400;
+  // get page height from video duration
+  var setHeight = document.getElementById("set-height");
+  // select video element
+  var vid = document.getElementById('v0');
+  // var vid = $('#v0')[0]; // jquery option
 
-var vid = document.getElementById('v0');
-var time = $('5');
-var scroll = $('150');
-var windowheight = $(window).height()-10;
+  // dynamically set the page height according to video length
+  vid.addEventListener('loadedmetadata', function() {
+    setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+  });
 
+  // Use requestAnimationFrame for smooth playback
+  function scrollPlay(){
+    var frameNumber  = Math.round(window.pageYOffset/playbackConst);
+    vid.currentTime  = frameNumber;
+  }
 
-var scrollpos = window.pageYOffset/10;
-var targetscrollpos = scrollpos;                              
-var accel = 0;
-
-
-// ---- Values you can tweak: ----
-var accelamount = 0.1; //How fast the video will try to catch up with the target position. 1 = instantaneous, 0 = do nothing.
-
-// pause video on load
-vid.pause();
- 
-window.onscroll = function(){
-    //Set the video position that we want to end up at:
-    targetscrollpos = window.pageYOffset/10;
-  
-    //move the red dot to a position across the side of the screen
-    //that indicates how far we've scrolled.
-    scroll.css('top', 10+(window.pageYOffset/130*windowheight));
-};
-
-
-setInterval(function(){  
-        
-      //Accelerate towards the target:
-      scrollpos += (targetscrollpos - scrollpos)*accelamount;
-  
-      //move the blue dot to a position across the side of the screen
-      //that indicates where the current video scroll pos is.  
-      time.css('top', 10+(scrollpos/130*10*windowheight));
-  
-      //update video playback
-      vid.currentTime = scrollpos;
-      vid.pause();
-    
-}, 40);
+  $(window).scroll(function() {
+    scrollPlay();
+  });
+});
